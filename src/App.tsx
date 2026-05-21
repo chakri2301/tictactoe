f41e5ab588f184d0fbaa0470a18a0fe195032a17
chakradhari <chakradhari.kothauppari@gmail.com>
2026-05-21 15:02:16 +0530

    styleFinalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
   const [values, setValues] = useState(new Array(9).fill(0));
-
-  return (<div id="board">{
-    values.map((ele)=>{
-      return <Square count={ele.toString()}/>
-    })}</div>)
+  function handleClick(idx:number){
+    let newValues = new Array(9).fill(0);
+    values.forEach(
+      (e, idx)=>{
+        newValues[idx] = e;
+      }
+    )
+    newValues[idx] = 2
+    setValues(newValues);
+  }
+  return (
+  <div id="app">
+  <div id="board">{
+    values.map((ele, index)=>{
+      return <Square idx={index} key={index} count={ele} onClick={handleClick}/>
+    })}
+    </div>
+  </div>
+  )
 }
-function Square({count}:{count:String}){
-  return<button>{count}</button>
+function Square({idx,count,onClick}:{idx:number,count:number,onClick:(idx:number)=>void}){
+  return<div className="wrapper"><span className={"tile"+ " " + "tile" + count}  onClick={()=>{onClick(idx);}}>{
+    (count==0)?" ":(count==1)?"X":"O"
+  }</span></div>
 }
 
 export default App
